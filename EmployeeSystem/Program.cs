0b7d03782d16d176d77b1ee635ba30fdd3f85d86
Son-JuHwan <sonjuhawn@gmail.com>
2026-03-03 17:54:41 +0900

    과제 완료

--- EmployeeSystem/Program.cs
+++ EmployeeSystem/Program.cs
@@ -1,4 +1,98 @@
 using System;
+using System.Runtime.InteropServices;
 
 // README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+Console.WriteLine("=== 직원 급여 시스템 ===\n");
+
+Manager manager = new Manager("김부장", 5000000, 1000000);
+manager.PrintInfo();
+Developer developer = new Developer("이개발", 4000000, 10);
+developer.PrintInfo();
+Intern intern = new Intern("박인턴", 1500000);
+intern.PrintInfo();
+
+class Employee
+{
+    protected string _name;
+    protected int _baseSalary;
+
+    public Employee(string name, int baseSalary)
+    {
+        _name = name;
+        _baseSalary = baseSalary;
+    }
+    public string Getname()
+    {
+        return _name;
+    }
+    public int GetbaseSalary()
+    {
+        return _baseSalary;
+    }
+    public int CalculatePay()
+    {
+        return _baseSalary;
+    }
+    public void PrintInfo()
+    {
+        Console.WriteLine($"{_name}");
+        Console.WriteLine($"기본급: {_baseSalary}");
+        Console.WriteLine($"총급여: {_baseSalary}");
+    }
+}
+class Manager : Employee
+{
+    public int _bonus;
+    public Manager(string name, int baseSalary, int bonus) : base(name, baseSalary)
+    {
+        _bonus = bonus;
+    }
+    public new int CalculatePay()
+    {
+        return _baseSalary + _bonus;
+    }
+    public new void PrintInfo()
+    {
+        Console.WriteLine($"[매니저] {_name}");
+        Console.WriteLine($"기본급: {_baseSalary}");
+        Console.WriteLine($"보너스: {_bonus}");
+        Console.WriteLine($"총급여: {CalculatePay()}");
+    }
+}
+
+class Developer : Employee
+{
+    public int _overtimeHours;
+    public Developer(string name, int baseSalary, int overtimehours) : base(name, baseSalary)
+    {
+        _overtimeHours = overtimehours;
+    }
+    public new int CalculatePay()
+    {
+        return _baseSalary + _overtimeHours * 20000;
+    }
+    public void PrintInfo()
+    {
+        Console.WriteLine($"[개발자] {_name}");
+        Console.WriteLine($"기본급: {_baseSalary}");
+        Console.WriteLine($"초과근무: {_overtimeHours}");
+        Console.WriteLine($"총급여: {CalculatePay()}");
+    }
+}
+class Intern : Employee
+{
+    public Intern(string name, int baseSalary) : base(name, 1500000)
+    {
+
+    }
+    public new int CalculatePay()
+    {
+        return _baseSalary;
+    }
+    public new void PrintInfo()
+    {
+        Console.WriteLine($"[인턴] {_name}");
+        Console.WriteLine($"기본급: {_baseSalary}");
+        Console.WriteLine($"총급여: {CalculatePay()}");
+    }
+}
--- ZooSimulator/Program.cs
+++ ZooSimulator/Program.cs
@@ -1,4 +1,92 @@
 using System;
+using System.Net.Sockets;
 
 // README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+Console.WriteLine("=== 동물원에 오신 것을 환영합니다! ===");
+
+Animal[] animal = new Animal[3];
+animal[0] = new Lion("시바", 5, "으르렁");
+Lion lion = (Lion)animal[0];
+animal[1] = new Elephant("덤보", 10, "뿌우");
+Elephant elephant = (Elephant)animal[1];
+animal[2] = new Penguin("뽀로로", 3, "꽥꽥");
+Penguin penguin = (Penguin)animal[2];
+
+Console.WriteLine("[동물 정보]");
+lion.DisplayInfo();
+elephant.DisplayInfo();
+penguin.DisplayInfo();
+
+Console.WriteLine("[동물 소리]");
+lion.MakeSound();
+elephant.MakeSound();
+penguin.MakeSound();
+
+Console.WriteLine("[동물 행동]");
+lion.Eat();
+lion.Hunt();
+elephant.Eat();
+elephant.SprayWater();
+penguin.Eat();
+penguin.Swim();
+
+
+class Animal
+{
+    public string Name;
+    public int Age;
+    protected string _sound;
+
+    public Animal(string name, int age , string sound)
+    {
+        Name = name;
+        Age = age;
+        _sound = sound;
+    }
+    public void Eat()
+    {
+        Console.WriteLine($"{Name}이(가) 먹이를 먹습니다");
+    }
+    public void MakeSound()
+    {
+        Console.WriteLine(_sound);
+    }
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"이름:{Name} 나이: {Age}");
+    }
+}
+class Lion : Animal
+{
+
+    public Lion (string name, int age, string sound) : base(name, age, sound)
+    {
+
+    }
+    public void Hunt()
+    {
+        Console.WriteLine($"{Name}이(가) 사냥을 합니다.");
+    }
+}
+class Elephant : Animal
+{
+    public Elephant(string name, int age, string sound) : base(name, age, sound)
+    {
+
+    }
+    public void SprayWater()
+    {
+        Console.WriteLine($"{Name}이(가) 물을 뿌립니다.");
+    }
+}
+class Penguin : Animal
+{
+    public Penguin(string name, int age, string sound) : base(name, age, sound)
+    {
+
+    }
+    public void Swim()
+    {
+        Console.WriteLine($"{Name}이(가) 수영을 합니다.");
+    }
+}
